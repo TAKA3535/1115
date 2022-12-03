1bc22c5f0c958d9853c008bddcadcb663060e205
takath <takun82586@gmail.com>
2022-12-03 20:13:18 +0900

    12/3

--- chap12/prac.c
+++ chap12/prac.c
@@ -13,13 +13,48 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+// int main(void){
+//     FILE *fp;
+
+//     fp = fopen("result.txt", "w");
+
+//     int i, j;
+
+//     if (fp == NULL){
+//         printf("ファイルをオープンできませんでした。\n");
+//         return 1;
+//     }
+//     else{
+//         printf("ファイルをオープンしました。\n");
+//     }
+
+//     for ( i = 1; i <= 5; i++){
+//         for ( j = 1; j <= 5; j++){
+//             fprintf(fp, "%-4d", i*j);
+//         }
+//         fprintf(fp, "\n");
+//     }
+//     printf("ファイルに書き込みました。\n");
+
+//     fclose(fp);
+//     printf("ファイルをクローズしました。\n");
+
+//     return 0;
+// }
+
+// 練習3
 #include <stdio.h>
-int main(void){
+
+int main(int argc, char *argv[]){
     FILE *fp;
-
-    fp = fopen("result.txt", "w");
-
     int i, j;
+
+    if (argc != 2){
+        printf("パラメータの数が違います。\n");
+        return 1;
+    }
+    fp = fopen(argv[1], "w");
 
     if (fp == NULL){
         printf("ファイルをオープンできませんでした。\n");
@@ -38,7 +73,6 @@ int main(void){
     printf("ファイルに書き込みました。\n");
 
     fclose(fp);
-    printf("ファイルをクローズしました。\n");
-
+    
     return 0;
 }
